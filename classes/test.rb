3b473ca16e9008a7f2906fa021165766135626b5
firstpj <114886262+firstpj@users.noreply.github.com>
2023-10-11 18:23:45 +0100

    classes files update

--- classes/test.rb
+++ classes/test.rb
@@ -1,12 +1,37 @@
-require_relative 'person'
-require_relative 'capitalize_decorator'
-require_relative 'trimmer_decorator'
+require_relative 'student'
+require_relative 'classroom'
+require_relative 'rental'
+require_relative 'book'
 
-person = Person.new(22, 'maximilianus')
-puts person.correct_name
+student1 = Student.new('Unknown', 13, 'John Baker')
+student2 = Student.new('Unknown', 17, 'Mark Adams')
+student3 = Student.new('Unknown', 14, 'George Austin')
 
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
+class1 = Classroom.new('Literature & Travels')
 
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+book1 = Book.new('First Class Travellers', 'Author1')
+book2 = Book.new('Flying Home', 'Author2')
+book3 = Book.new('Striking UPS Workers', 'Author3')
+
+Rental.new('2017-12-07', book1, student1)
+Rental.new('2018-03-07', book2, student2)
+Rental.new('2019-08-07', book2, student1)
+Rental.new('2022-02-09', book3, student3)
+
+puts class1.add_student(student1)
+puts student1.classroom.label
+puts student2.classroom
+puts class1.students.count
+puts student2.classroom = class1
+puts class1.students.count
+
+puts book1.rentals
+puts book2.rentals
+puts book3.rentals
+
+puts student1.rentals.count
+puts student2.rentals.count
+puts student3.rentals.count
+puts book1.rentals.count
+puts book2.rentals.count
+puts book3.rentals.count
